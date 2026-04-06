bc62c3c9384315c062b1d6cd4e41dc4e00b942bd
Prayag-raj-sharma <86906091+Prayag-raj-sharma@users.noreply.github.com>
2026-04-06 16:41:05 +0530

    Added code for rate liming and swagger integration

--- AIpoc/src/main/java/com/example/AIpoweredSystem/controller/AIController.java
+++ AIpoc/src/main/java/com/example/AIpoweredSystem/controller/AIController.java
@@ -4,6 +4,8 @@ import com.example.AIpoweredSystem.dto.ApiResponse;
 import com.example.AIpoweredSystem.dto.LogResponse;
 import com.example.AIpoweredSystem.entity.AIQueryLog;
 import com.example.AIpoweredSystem.service.IAIService;
+import com.example.AIpoweredSystem.service.ratelimiter.RateLimiterService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +17,15 @@ import java.util.concurrent.CompletableFuture;
 @RequestMapping("/ai")
 public class AIController {
     private final IAIService aiService;
+    private final RateLimiterService rateLimiterService;
 
     @GetMapping("/ask")
-    public CompletableFuture<ApiResponse<String>> askAI(@RequestParam String query) {
+    public CompletableFuture<ApiResponse<String>> askAI(@RequestParam String query, HttpServletRequest request) {
+        String clientIp = request.getRemoteAddr();
+
+        if (!rateLimiterService.isAllowed(clientIp)) {
+            throw new RuntimeException("Rate limit exceeded");
+        }
         return aiService.askAI(query)
                 .thenApply(response->
                         new ApiResponse<>(true, response));
